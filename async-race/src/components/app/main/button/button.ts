ff34f65f39eb21fd6496faf729ddce362d6660a8
Anna Coração <lazakovich@gmail.com>
2024-01-23 19:03:40 +0300

    refactor: delete unnecessary variable in button module

--- async-race/src/components/app/main/button/button.ts
+++ async-race/src/components/app/main/button/button.ts
@@ -6,15 +6,13 @@ class Button {
 
   private buttonInner: HTMLDivElement;
 
-  private text: HTMLSpanElement;
-
   constructor(container: HTMLDivElement, name: string, id?: number) {
     this.button = createElement("div", ["button"], container);
     if (name === "UPDATE" || name === "RESET" || name === "Land") {
       this.button.classList.add("inactive");
     }
     this.buttonInner = createElement("div", ["buttonInner"], this.button);
-    this.text = createElement("span", ["buttonText"], this.buttonInner, name);
+    createElement("span", ["buttonText"], this.buttonInner, name);
     this.button.setAttribute("id", String(id));
   }
 }
